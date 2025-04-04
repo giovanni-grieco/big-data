fb2addff2664ed7f9138bb313dca19359d6e3459
giovanni <giovi.5057@gmail.com>
2025-04-04 11:12:45 +0200

    added metastore_db wipe option

--- setup.sh
+++ setup.sh
@@ -191,8 +191,20 @@ echo "HIVE config done"
 
 hdfs namenode -format
 
-current_path=$(pwd)
 
-schematool -dbType derby -initSchema
+#Ask user if they want to wipe the metastore_db folder
+if [ -d "metastore_db/" ]; then
+    read -p "Do you want to wipe the metastore_db folder? (y/n): " wipe_metastore
+    if [[ $wipe_metastore == "y" || $wipe_metastore == "Y" ]]; then
+        echo "Wiping metastore_db folder..."
+        rm -rf "metastore_db/"
+        schematool -dbType derby -initSchema
+    else
+        echo "Not wiping metastore_db folder."
+    fi
+else
+    echo "metastore_db folder not found. Initializing schema..."
+    schematool -dbType derby -initSchema
+fi
 
 echo "All done!"

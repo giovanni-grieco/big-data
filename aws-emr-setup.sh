82837032d58f68e7f1d586b1870de164d86dccfb
giovanni <giovi.5057@gmail.com>
2025-06-03 19:02:57 +0200

    updated to python3

--- aws-emr-setup.sh
+++ aws-emr-setup.sh
@@ -22,7 +22,7 @@ if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
     python3 -m venv .venv
     source .venv/bin/activate
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     echo "Virtual environment already exists."
     source .venv/bin/activate

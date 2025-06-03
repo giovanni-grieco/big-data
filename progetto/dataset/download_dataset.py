9d1aa5c0f2e0b7bf6710d69f6343c3f978d0eb58
giovanni <giovi.5057@gmail.com>
2025-06-03 11:19:21 +0200

    cluster

--- progetto/dataset/download_dataset.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("ananaymital/us-used-cars-dataset")
-
-print("Path to dataset files:", path)
---
+++ progetto/dataset/download_dataset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ~/Downloads/us-used-cars-dataset.zip\
+  https://www.kaggle.com/api/v1/datasets/download/ananaymital/us-used-cars-dataset

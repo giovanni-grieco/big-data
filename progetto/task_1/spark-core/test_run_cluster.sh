0aa95d6d63a73f4800fe1128458e58d8748e7812
giovanni <giovi.5057@gmail.com>
2025-06-05 16:36:53 +0200

    test

--- progetto/task_1/spark-core/test_run_cluster.sh
+++ progetto/task_1/spark-core/test_run_cluster.sh
@@ -5,5 +5,5 @@ spark-submit \
         --executor-memory 4g \
         --executor-cores 4 \
         spark-job.py \
-        -i hdfs://localhost:9000/user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv \
-        -o hdfs://localhost:9000/user/$USER/output/task1_spark_test_result \
+        -i hdfs://ec2-18-195-217-145.eu-central-1.compute.amazonaws.com:9000/user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv \
+        -o hdfs://ec2-18-195-217-145.eu-central-1.compute.amazonaws.com:9000/user/$USER/output/task1_spark_test_result \

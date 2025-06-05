f13588304fbfb18d4841ca416df25c3e3cd680fe
giovanni <giovi.5057@gmail.com>
2025-06-05 16:31:56 +0200

    fixato script

--- progetto/task_1/spark-core/test_run_cluster.sh
+++ progetto/task_1/spark-core/test_run_cluster.sh
@@ -1,5 +1,9 @@
 spark-submit \
         --master yarn \
+        --deploy-mode cluster \
+        --driver-memory 4g \
+        --executor-memory 4g \
+        --executor-cores 4 \
         spark-job.py \
         -i hdfs://localhost:9000/user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv \
         -o hdfs://localhost:9000/user/$USER/output/task1_spark_test_result \

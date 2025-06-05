03f203b68cdfbd128b5619ba4fd6cee52b42580e
giovanni <giovi.5057@gmail.com>
2025-06-05 13:59:35 +0200

    aggiornato script a nuova API

--- progetto/task_1/mapreduce/test_run_cluster.sh
+++ progetto/task_1/mapreduce/test_run_cluster.sh
@@ -1,7 +1,6 @@
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -input /user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv \
         -output /user/$USER/output/task1_mapreduce_test_result \
-        -file ./mapper.py \
-        -file ./reducer.py \
+        -files ./mapper.py,./reducer.py \
         -mapper mapper.py \
         -reducer reducer.py \

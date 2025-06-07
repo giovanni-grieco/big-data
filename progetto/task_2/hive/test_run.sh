63ee4ec582b8b25d3404dd7ce7623842007d408c
giovanni <giovi.5057@gmail.com>
2025-06-07 21:34:51 +0200

    Modalità di esecuzione del test_run in modo che stampi tutti i log

--- progetto/task_2/hive/test_run.sh
+++ progetto/task_2/hive/test_run.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+# Clean up previous outputs
+hdfs dfs -rm -r -f /task_2
 hdfs dfs -mkdir -p /task_2/hive/
 hdfs dfs -put extract_top_words.py /task_2/hive/
 
-hive -hivevar input_path=/user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv -f ./task.hql
+# Enable verbose logging and save output
+hive -hivevar input_path=/user/$USER/input/cleaned_pruned_used_cars_data_1percent.csv \
+     -hiveconf hive.root.logger=INFO,console \
+     -hiveconf mapreduce.job.queuename=default \
+     -f ./task.hql 2>&1 | tee hive_run_$(date +%Y%m%d_%H%M%S).log
